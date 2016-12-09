ca19782ec884d9aef337cd1102b6594248960dc1
i310934 <j.pan@sap.com>
2016-12-09 19:39:20 +0800

    add line bridge

---
+++ src/analytics/fft/bridge.js
@@ -0,0 +1,21 @@
+
+    const bridgeScene = new bridge3d.BridgeScene({
+        $element: $("div#chuhe-bridge"),
+        bridgeModelUrl: "/models/bridge.obj"
+    });
+    bridgeScene.startAnimation();
+
+    bridgeScene.on("load", function(e) {
+        // this.bridge.showSensors(sensorIds);
+    });
+
+    bridgeScene.on("sensorclick", function(e) {
+        this.bridge.focusOnSensor(e.sensor);
+    });
+
+    $(window).on("resize", function(e) {
+        bridgeScene.invalidateSize();
+    });
+
+
+     module.exports= bridgeScene;
---
+++ src/analytics/fft/timelinechart.js
@@ -0,0 +1,75 @@
+const series = {
+  data: [[(new Date(2000, 10, 1)).getTime(), 3],[(new Date(2000, 11, 1)).getTime(), 4],[(new Date(2000, 12, 1)).getTime(), 6]],
+  color: 'green',
+  fill: true,
+  fillColor: {colors: ['rgb(41, 176, 146)', 'rgb(64, 112, 138)']}
+};
+
+const lineChart = $(".chuhe-time-linechart > .chuhe-linechart-content").plot([series], {
+    series: {
+      lines: {
+          show: true
+      },
+      points: {
+          show: true
+      }
+    },
+    zoom: {
+        interactive: false
+    },
+    pan: {
+        interactive: false
+    },
+    xaxis: {
+      mode: 'time',
+      show: true,
+      font: {
+          color: 'white'
+      },
+      tickFormatter: function(value)
+      {
+          var d = new Date(value);
+          return `${d.getFullYear()}- ${d.getMonth()}-${d.getDate()}`;
+      }
+    },
+    yaxis: {
+        show: true,
+        zoomRange: false,
+        panRange: false,
+        font: {
+            color: 'white'
+        }
+    },
+    grid: {
+        show: true,
+        color: '#9b99ff',
+        axisMargin: '15px',
+        borderWidth: {
+            left: 1,
+            bottom: 1,
+            top: 0,
+            right: 0
+        },
+        margin: {
+            left: 15,
+            right: 15,
+            top: 60,
+            bottom: 5
+        },
+        borderColor: {
+            left: '#9b99ff',
+            bottom: '#9b99ff'
+        }
+    }
+}).data('plot');
+
+$(window).on('resize', function() {
+    lineChart.resize();
+    lineChart.setupGrid();
+    lineChart.draw();
+});
+
+module.exports = {
+  linechart: lineChart,
+  series: series
+}
