851ca93debe5b59733176f7612398825e48979f9
haiyanx <1363745435@qq.com>
2016-12-27 17:44:05 +0800

    test

--- src/analytics/alarmanalytic/index.js
+++ src/analytics/alarmanalytic/index.js
@@ -1,1 +1,9 @@
 require('./style.less');
+$("input#chuhe-alarmAnalytics-submit").on('click', e => {
+    series.data = [{ label: "Series1", data: 10}, { label: "Series2", data: 30}, { label: "Series3", data: 90}, { label: "Series4", data: 70},
+        { label: "Series5", data: 80}, { label: "Series6", data: 110}]
+
+    linechart.setData([series]);
+    linechart.setupGrid();
+    linechart.draw();
+})
