2eb73a935ab92124444bdf9a7c5de89d596d5fca
haiyan <1363745435@qq.com>
2017-01-19 11:23:53 +0800

    change 24 hour

--- src/monitor/deflection/html.js
+++ src/monitor/deflection/html.js
@@ -3,9 +3,9 @@ const content = require("./content.ejs");
 const monitor = require("../../component/monitor/html.js");
 
 module.exports = layout.generate(content({
-  monitor: monitor({
-    name: '位移',
-    unit: 'mm',
-    values: ['displacement']
-  })
+    monitor: monitor({
+        name: '挠度',
+        unit: 'mm',
+        values: ['deflection'],
+    }),
 }));
